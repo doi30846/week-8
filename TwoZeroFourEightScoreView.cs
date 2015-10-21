cdfbd0a09d9f9aad23c1292c86c25de65e640c38
doi30846 <doi30846@gmail.com>
2015-10-21 18:32:59 +0700

    new current score form 1013

---
+++ TwoZeroFourEightScoreView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace twozerofoureight
+{
+    public partial class TwoZeroFourEightScoreView : Form
+    {
+       
+        private string pass;
+
+        public string Passvalue
+        {
+            get { return pass; }
+            set { pass = value; }
+        }
+
+            
+        public TwoZeroFourEightScoreView()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void TwoZeroFourEightScoreView_Load(object sender, EventArgs e)
+        {
+            lblScore.Text = "Your score = " +pass;
+        }
+    }
+}
